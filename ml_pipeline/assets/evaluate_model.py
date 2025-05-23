54c885c478137aacd8e00dee2b89c6146e969d63
Linda Lacsivy <lindalacsivy@outlook.de>
2025-05-23 11:09:32 +0200

    rand_forest_model() done, eval_rand_forest() done

--- ml_pipeline/assets/evaluate_model.py
+++ ml_pipeline/assets/evaluate_model.py
@@ -1,20 +1,35 @@
 from dagster import asset
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from ml_pipeline.utils.eval_metrics import compute_eval_metrics
 
 @asset
 def eval_lin_reg(lin_reg_model: LinearRegression, test_data: tuple) -> dict:
     """
-    
+
     Args:
-        lin_reg_model: 
-        test_data: 
+        lin_reg_model:
+        test_data:
 
     Returns: eval_metrics
 
     """
     _, X_test, _, y_test = test_data
     return compute_eval_metrics(lin_reg_model, X_test, y_test)
+
+@asset
+def eval_rand_forest(rand_forest_model: RandomForestRegressor, test_data: tuple) -> dict:
+    """
+    
+    Args:
+        rand_forest_model: 
+        test_data: 
+
+    Returns:
+
+    """
+    _, X_test, _, y_test = test_data
+    return compute_eval_metrics(rand_forest_model, X_test, y_test)
 
 
 
--- ml_pipeline/assets/train_model.py
+++ ml_pipeline/assets/train_model.py
@@ -5,24 +5,36 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 @asset
-def split_data(bikes_features: pd.DataFrame) -> tuple:
+def train_data(bikes_features: pd.DataFrame) -> tuple:
+    """
+
+    Args:
+        bikes_features:
+
+    Returns: train data
+
+    """
     X = bikes_features.drop(columns=['cnt'])
     y = bikes_features['cnt']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    print(X_train.shape, X_test.shape)
-    return  X_train, X_test, y_train, y_test
-
-@asset
-def train_data(split_data: tuple) -> tuple:
-    X_train, _, y_train, _ = split_data
+    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=42)
     print(X_train.shape)
-    return   X_train, _, y_train, _
+    return  X_train, _, y_train, _
 
 @asset
-def test_data(split_data: tuple) -> tuple:
-    X_test, _, y_test, _ = split_data
+def test_data(bikes_features: pd.DataFrame) -> tuple:
+    """
+
+    Args:
+        bikes_features:
+
+    Returns: test data
+
+    """
+    X = bikes_features.drop(columns=['cnt'])
+    y = bikes_features['cnt']
+    _, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     print(X_test.shape)
-    return   X_test, _, y_test, _
+    return  _, X_test, _, y_test
 
 @asset
 def lin_reg_model(train_data: tuple) -> LinearRegression:
@@ -40,23 +52,23 @@ def lin_reg_model(train_data: tuple) -> LinearRegression:
     print("Linear Regression Model Trained")
     return model
 
-# @asset
-# def rand_forest_model(train_data: tuple) -> RandomForestRegressor:
-#     """
-#     
-#     Args:
-#         train_data: 
-# 
-#     Returns: random forest model
-# 
-#     """
-#     X_train, _, y_train, _ = train_data
-#     model = RandomForestRegressor(random_state=42)
-#     param_grid = {
-#         'n_estimators': [100, 200],
-#         'max_depth': [10, 20],
-#     }
-#     grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
-#     grid_search.fit(X_train, y_train)
-#     print("Random Forest Model Trained")
-#     return grid_search.best_estimator_
+@asset
+def rand_forest_model(train_data: tuple) -> RandomForestRegressor:
+    """
+
+    Args:
+        train_data: 
+
+    Returns: random forest model
+
+    """
+    X_train, _, y_train, _ = train_data
+    model = RandomForestRegressor(random_state=42)
+    param_grid = {
+        'n_estimators': [100, 200],
+        'max_depth': [10, 20],
+    }
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+    grid_search.fit(X_train, y_train)
+    print("Random Forest Model Trained")
+    return grid_search.best_estimator_
