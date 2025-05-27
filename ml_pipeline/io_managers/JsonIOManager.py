00ac6ebfedaf8ac504fb99af4dedd1687e91b119
Linda Lacsivy <lindalacsivy@outlook.de>
2025-05-27 17:17:08 +0200

    put json files into storage/evals folder

--- ml_pipeline/io_managers/JsonIOManager.py
+++ ml_pipeline/io_managers/JsonIOManager.py
@@ -3,12 +3,14 @@ import json
 from dagster import IOManager, io_manager, OutputContext, InputContext
 
 class JsonIOManager(IOManager):
-    def handle_output(self, context: OutputContext, obj):
-        base_path = "storage"
-        filename = f"{'_'.join(context.asset_key.path)}.json"
-        output_path = os.path.join(base_path, filename)
+    def __init__(self, base_path: str):
+        self.base_path = base_path
 
-        os.makedirs(base_path, exist_ok=True)
+    def handle_output(self, context: OutputContext, obj):
+        filename = f"{'_'.join(context.asset_key.path)}.json"
+        output_path = os.path.join(self.base_path, filename)
+
+        os.makedirs(self.base_path, exist_ok=True)
 
         with open(output_path, "w") as f:
             json.dump(obj, f)
@@ -16,14 +18,14 @@ class JsonIOManager(IOManager):
         context.log.info(f"Stored JSON output to {output_path}")
 
     def load_input(self, context: InputContext):
-        base_path = "storage"
         filename = f"{'_'.join(context.asset_key.path)}.json"
-        input_path = os.path.join(base_path, filename)
+        input_path = os.path.join(self.base_path, filename)
 
         with open(input_path, "r") as f:
             return json.load(f)
 
 
-@io_manager
-def json_io_manager():
-    return JsonIOManager()
+@io_manager(config_schema={"base_path": str})
+def json_io_manager(init_context):
+    base_path = init_context.resource_config["base_path"]
+    return JsonIOManager(base_path=base_path)
